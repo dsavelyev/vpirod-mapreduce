58f925279a68e6593dbeaa94eeb939e081fe67f8
Dmitry Savelyev <ds2012.65@gmail.com>
2017-12-05 20:21:25 +0300

    Add sherlock

---
+++ sherlock.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+
+import csv
+import glob
+import os.path
+import sys
+
+import mincemeat as mm
+
+
+def readall(fname):
+    with open(fname, 'ru') as f:
+        return f.read()
+
+
+def run_server(docs, docnames):
+    rdr = {docname: readall(fname) for fname, docname in zip(docs, docnames)}
+
+    s = mm.Server()
+    s.datasource = rdr
+    s.mapfn = mapfn
+    s.reducefn = eval(reducefn_template % docnames)
+
+    return s.run_server()
+
+
+def mapfn(k, v):
+    import nltk.tokenize as tkz
+
+    for w in tkz.wordpunct_tokenize(v):
+        yield w.lower(), k
+
+
+reducefn_template = 'lambda k, vs: map(vs.count, %s)'
+
+
+if __name__ == '__main__':
+    docs = glob.glob(os.path.join(sys.argv[1], '*'))
+    docnames = [os.path.splitext(os.path.basename(x))[0] for x in docs]
+
+    out = run_server(docs, docnames)
+
+    with open(sys.argv[2], 'wu') as outf:
+        wr = csv.writer(outf)
+        wr.writerow(['Term'] + docnames)
+        wr.writerows([[k] + v for k, v in out.iteritems()])
