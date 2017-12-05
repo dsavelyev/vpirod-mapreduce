dc2b8c5bb8a9ca05000c1915c6339b2ac11c83f9
Dmitry Savelyev <ds2012.65@gmail.com>
2017-12-05 11:08:38 +0300

    Initial commit

---
+++ southpark.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+import csv
+import sys
+
+import mincemeat as mm
+
+
+def run_server(csv_file):
+    rdr = dict(enumerate(csv.DictReader(csv_file)))
+
+    s = mm.Server()
+    s.datasource = rdr
+    s.mapfn = mapfn
+    s.reducefn = reducefn
+
+    return s.run_server()
+
+
+def mapfn(k, v):
+    import nltk.tokenize as tkz
+
+    char, line = v['Character'], v['Line']
+    for w in tkz.wordpunct_tokenize(line):
+        yield char, w.lower()
+
+
+def reducefn(k, vs):
+    return len(set(vs))
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        out = run_server(f)
+
+    with open(sys.argv[2], 'w') as outf:
+        csv.writer(outf).writerows(out.items())
